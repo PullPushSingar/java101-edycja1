b950fdcf6057d3925302222c00af71e96e74c33a
PullPushSingar <105585719+PullPushSingar@users.noreply.github.com>
2023-03-05 20:10:06 +0100

    Task 3 has benn added

--- w2/zadania/PullPushSingar/Zadanie3.java
+++ w2/zadania/PullPushSingar/Zadanie3.java
@@ -1,72 +1,46 @@
 package w1.zadania.PullPushSingar;
 
-public class Zadanie1 {
+import java.io.*;
 
-    public static void main(String[] args) {
-        int a,b,c,d,ab,cd;
-        double e,f,g,h,ef,gh;
-        char i,j;
-        String k,l;
-        boolean m,n;
+public class Zadanie3 {
+    public static void main(String[] args) throws IOException {
+        Reader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-        a = 1;
-        b = 2;
-        c = 3;
-        d = 4;
-
-        ab = a + b;
-        cd = c - d;
-
-        e = 1.1;
-        f = 2.2;
-        g = 3.3;
-        h = 4.4;
-
-        ef = e * f;
-        gh = g/h;
+        System.out.println("Please Write first trangle side : ");
+        String firstSideString = bufferedReader.readLine();
+        System.out.println("Please Write first second side : ");
+        String secondSideString = bufferedReader.readLine();
+        System.out.println("Please Write first third side : ");
+        String thirdSideString = bufferedReader.readLine();
 
 
+        int firstSide = Integer.parseInt(firstSideString);
+        int secondSide = Integer.parseInt(secondSideString);
+        int thirdSide = Integer.parseInt(thirdSideString);
 
-        i = 'A';
-        j = 'a';
-
-
-
-        k = "Ala";
-        l = "ma";
-
-
-
-        m = true;
-        n = false;
-
-        System.out.println("Wartosć zmiennej a to :" + a);
-        System.out.println("Wartosć zmiennej b to :" + b);
-        System.out.println("Wartosć zmiennej c to :" + c);
-        System.out.println("Wartosć zmiennej d to :" + d);
-        System.out.println("Wartosć zmiennej ab to :" + ab);
-        System.out.println("Wartosć zmiennej cd to :" + cd);
-        System.out.println("Wartosć zmiennej e to :" + e);
-        System.out.println("Wartosć zmiennej f to :" + f);
-        System.out.println("Wartosć zmiennej g to :" + g);
-        System.out.println("Wartosć zmiennej h to :" + h);
-        System.out.println("Wartosć zmiennej ef to :" + ef);
-        System.out.println("Wartosć zmiennej gh to :" + gh);
-        System.out.println("Reszta z dzielenia ab/b = " + ab % b);
-        System.out.println("Wartosć zmiennej i to :" + i);
-        System.out.println("Wartosć zmiennej j to :" + j);
-        System.out.println("Wynik dodania i,j to :" + (j + i));
-        System.out.println("Wartosć zmiennej k to :" + k);
-        System.out.println("Wartosć zmiennej l to :" + l);
-        System.out.println("Wynik dodania zmiennych k i l to " + (k + l));
-        System.out.println("Wartosć zmiennej m to :" + m);
-        System.out.println("Wartosć zmiennej n to :" + n);
-        System.out.println("Wynik m && n to :" + (m && n));
+        if(!canBuildTriangle(firstSide,secondSide,thirdSide)){
+            System.out.println("A triangle cannot be constructed.");
+            System.exit(2137);
+        }
+        if(isEquilateral(firstSide,secondSide,thirdSide)){
+            System.out.println("triangle is equilateral.");
+        } else if (isIsosceles(firstSide,secondSide,thirdSide)) {
+            System.out.println("triangle is isosceles.");
+            
+        } else {
+            System.out.println("The triangle is scalene.");
+        }
 
 
-
-
-
-
+    }
+    public static boolean canBuildTriangle(int a , int b , int c){
+        return  (a < c + b) &&  (c < a + b) && (b < c + a);
+    }
+    public static boolean isEquilateral(int a , int b , int c){
+        return  a == b && b == c && c == a;
+    }
+    public static boolean isIsosceles(int a , int b , int c){
+        return  (a == b && a != c) || (a == c && a != b ) || (b == c && b != a);
     }
 }
